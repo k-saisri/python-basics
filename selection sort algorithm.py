3dfb73763842d425c845f41f5e22793113dfeac8
k-saisri <167082109+k-saisri@users.noreply.github.com>
2024-04-28 12:20:43 +0530

    Create selection sort algorithm.py

---
+++ selection sort algorithm.py
@@ -0,0 +1,20 @@
+def performSelectionSort(nums):
+    n = len(nums)
+    for fixThisIndex in range(n - 1, 0, -1):
+        maxEle = nums[fixThisIndex]
+        maxEleIndex = fixThisIndex 
+ 
+        for index in range(fixThisIndex):
+            if nums[index] > maxEle:
+                maxEleIndex = index 
+                maxEle = nums[index]
+        if fixThisIndex != maxEleIndex:
+            temp = nums[maxEleIndex]
+            nums[maxEleIndex] = nums[fixThisIndex]
+            nums[fixThisIndex] = temp
+ 
+        # print(nums)
+n=int(input())
+nums=list(map(int,input().split()))
+performSelectionSort(nums)
+print(*nums)
